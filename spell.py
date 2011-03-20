28bcf03ec87588f24c94d2e9be75c60395b3bc64
JoyHui <joy.chenhuan.hui@gmail.com>
2011-03-20 14:29:17 -0600

    Initial commit

---
+++ spell.py
@@ -0,0 +1,90 @@
+ALPH = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ"
+def build_counter(textfile):
+	# "counts" the numbers in the file
+	words = {}
+	for number in textfile:
+		number = number.strip()
+		if not number in words:
+			words[number]=1
+		else:
+			 words[number]+= 1
+	return words
+def check_word(word, dictionary):
+	# checks if a word is in a previously made dictionary
+	return word in dictionary
+
+def check_list(words, dictionary):
+	real_words=[]
+	for word in words:
+		if check_word(word, dictionary):
+			real_words.append(word)
+	return real_words
+
+def check_char_sub(wrongword, dictionary):
+	poss_list=[]
+	for i in range(0,len(wrongword)):
+		s1 = wrongword[0:i]
+		s2 = wrongword[ i+1: len(wrongword)]
+		for letter in ALPH:
+			#print s1 + letter + s2
+			if s1 + letter + s2 == "cod":
+				print check_word("cod", dictionary)
+			if check_word(s1 +letter +s2, dictionary):
+				poss_list.append(s1 + letter +s2)
+	return poss_list
+	
+def word_part(word):
+	partitions= []
+	for i in range(0, len(word)):
+		#(below) partitioning word, putting it in array, putting that array
+		#in partitions
+		#button turns into [ ['b', 'utton'],...]
+		partitions.append([word[0:i+1],word[i+1:len(word)]])
+	return partitions
+	
+def sub_list(partitions):
+	corrections= []
+	for i in range(0, len(partitions)):
+		first_slice=partitions[i][0]
+		length= len(first_slice)
+		if length < 1:
+			continue
+		first_slice=first_slice[0:length-1]
+		second_slice=partitions[i][1]
+		for letter in ALPH:
+			corrections.append(first_slice+letter+second_slice)
+	return corrections
+
+def del_list(partitions):
+	corrections=[]
+	for letter in ALPH:
+		corrections.append(letter + partitions[0][0] + partitions[0][1])
+	for i in range(0, len(partitions)):
+		for letter in ALPH:
+			corrections.append(partitions[i][0]+letter+partitions[i][1])
+	return corrections
+
+def add_list(partitions):
+	corrections=[]
+	for i in range(0, len(partitions)):
+		first_slice=partitions[i][0]
+		length = len(first_slice)
+		if length < 1:
+			continue
+		first_slice=first_slice[0:length -1]
+		second_slice=partitions[i][1]
+		corrections.append(first_slice+second_slice)
+	return corrections
+
+
+
+def suggest(word):
+	f=open("corpus", "r")
+	histogram = build_counter(f)
+	partitions = word_part(word)
+	corrections  = set(add_list(partitions)+del_list(partitions)+ sub_list(partitions))
+	corrections= check_list(corrections,histogram)
+	
+#print check_char_sub("grandsonn", histogram)
+#partitions = word_part("button")
+print suggest("cow")
