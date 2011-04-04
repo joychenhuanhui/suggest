c2cd8b9f632240b8388a58af67456fcb324e4351
Joy Chenhuan Hui <joy.chenhuan.hui@gmail.com>
2011-04-04 11:10:05 -0600

    Add appling2 trainer

--- error_model_helpers.py
+++ error_model_helpers.py
@@ -1,5 +1,5 @@
 TPATH = "0643/0643/"
-TRAIN = ["ABODAT.643", "APPLING1DAT.643"]
+TRAIN = ["ABODAT.643", "APPLING1DAT.643", "APPLING2DAT.643"]
 
 # @brief Returns summary of errors for the minimum edit distance of two words
 def minimum_edits(s, t):
@@ -57,10 +57,24 @@ def train_appling1dat(path, model):
 			model[error] += 1
 	return model
 
+def train_appling2dat(path, model):
+	f = open(path, "r")
+	for line in f:
+		if line[0] == "$":
+			continue
+		words = line.split()
+		if len(words) != 2:
+			continue
+		for error in minimum_edits(words[0], words[1]):
+			model[error] += 1
+	return model
+
 def error_model():
 	model = {"D":0, "I":0, "R":0}
 	train_abodat(TPATH+TRAIN[0], model)
 	train_appling1dat(TPATH+TRAIN[1], model)
+	train_appling2dat(TPATH+TRAIN[2], model)
+
 	return model
 
 print error_model()
