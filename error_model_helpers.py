6c10b0ecb35768b4b11bad06d84ee081cc96e34d
Joy Chenhuan Hui <joy.chenhuan.hui@gmail.com>
2011-04-04 09:53:37 -0600

    Implement error counting in minimum edit distance method

--- error_model_helpers.py
+++ error_model_helpers.py
@@ -3,22 +3,33 @@ def minimum_edits(s, t):
 	m = len(s)+1
 	n = len(t)+1
 	d = [[-1]*(n) for x in range(0,m)]
-	summary = [[-1]*(n) for x in range(0,m)]
-	
+	summary = [[[]]*(n) for x in range(0,m)]
+
 	for i in range(0,m):
 		d[i][0] = i
-		summary = ("ADD")
+		summary[i][0] = ["I"]
 	for j in range(0,n):
 		d[0][j] = j
-		summary = ("ADD")
+		summary[0][j] = ["I"]
 
 	for j in range(1,n):
 		for i in range(1,m):
 			if s[i-1] == t[j-1]:
 				d[i][j] = d[i-1][j-1]
+				summary[i][j] = summary[i-1][j-1]
 			else:
 				d[i][j] = min(
 				d[i-1][j]+1,
 				d[i][j-1]+1,
 				d[i-1][j-1]+1)
+
+				if d[i-1][j]+1 <= d[i][j-1]+1 and d[i-1][j]+1 <= d[i-1][j-1]+1:
+					summary[i][j] = summary[i-1][j] + ["D"]
+				elif d[i][j-1]+1 <= d[i-1][j]+1 and d[i][j-1]+1 <= d[i-1][j-1]+1:
+					summary[i][j] = summary[i][j-1] + ["I"]
+				elif d[i-1][j-1]+1 <= d[i-1][j]+1 and d[i-1][j-1]+1 <= d[i][j-1]+1:
+					summary[i][j] = summary[i-1][j-1] + ["R"]
 	print summary
+	print summary[m-1][n-1]
+
+minimum_edits("exponential", "polynomial")
