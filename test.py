ac43e03b3529c0a3af2762df9e98334cca814b38
Joy Chenhuan Hui <joy.chenhuan.hui@gmail.com>
2011-04-04 19:19:49 -0600

    Add comparison to Norvig algorithm

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import scramble_helpers
+import scramble_helpers, norvig
 
 def build_tests():
 	similarity_model = scramble_helpers.similarity_model("corpus")
@@ -7,16 +7,22 @@ def build_tests():
 
 	f = open("test_errors", "r")
 	our_correct = 0
+	norvig_correct = 0
 	count = 0
 	for line in f:
 		pair = line.split()
 		if len(pair[0]) < 6 or len(pair[1]) < 6:
 			continue
-		out = scramble_helpers.suggest(pair[1],char_model,similarity_model, error_model)
-		if out[0].lower() == pair[0].lower():
+		our_out = scramble_helpers.suggest(pair[1],char_model,similarity_model, error_model)
+		norvig_out = norvig.correct(pair[1].lower())
+		if our_out[0].lower() == pair[0].lower():
 			our_correct += 1
+		if norvig_out.lower() == pair[0].lower():
+			norvig_correct += 1
 		count += 1
-		print (out[0], pair[1],  pair[0])
-	print our_correct/float(count)
+		#print (our_out[0], pair[1],  pair[0])
+	print count
+	print "our correct: " + str(our_correct/float(count))
+	print "norvig correct: " + str(norvig_correct/float(count))
 
 build_tests()
